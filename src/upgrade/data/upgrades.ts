7b8a826a6a6fc067262762313543db4b1f0734c5
XORs33r <bugislife@proton.me>
2024-07-09 21:29:54 +0800

    fix: lab title and image

--- src/upgrade/data/upgrades.ts
+++ src/upgrade/data/upgrades.ts
@@ -64,13 +64,13 @@ export const upgradesData: UpgradesCategory[] = [
     upgrades: [
       {
         id: 4,
-        title: "Weed_lab",
+        title: "Weed lab",
         description: "Start your weed production",
         level: 0,
         maxLevel: 5,
         levelPrices: [200, 400, 600, 800, 1600, 3200],
         value: [1.1, 1.2, 1.3, 1.4, 1.42, 1.45],
-        image: "/assets/weed_lab_small.png",
+        image: "/assets/weed_lab_small.webp",
         locked: false,
         group: "production",
         requirement: null,

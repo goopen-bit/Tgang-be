884b4597e6b60e9c0cbb06b20adf55ab47a4f4e4
XORs33r <bugislife@proton.me>
2024-07-13 12:02:51 +0800

    feat: remove reputation for price calculation

--- src/market/market.service.ts
+++ src/market/market.service.ts
@@ -16,14 +16,15 @@ export class MarketService {
     const market = await this.getMarket(marketId);
     const user = await this.userService.findOne(userId);
     market.products.forEach((product) => {
-      product.price = Math.floor(product.price * (1 - user.reputation / 100));
+      // @note remove reputation for now
+      // product.price = product.price * (1 - user.reputation / 100);
       const dealerUpgrade = user.upgrades.find(
         (upgrade) =>
           upgrade.group === "product" && upgrade.title === product.name
       );
       if (dealerUpgrade) {
         const discount = dealerUpgrade.value[dealerUpgrade.level];
-        product.price = Math.floor(product.price * discount);
+        product.price = product.price * discount;
       }
     });
 

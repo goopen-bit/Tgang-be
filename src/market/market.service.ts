2468e234fd8ded9eb54410d6bbdeaf58f5affc20
XORs33r <bugislife@proton.me>
2024-07-13 12:27:17 +0800

    feat: create a deep copy of market to avoid modifying original data

--- src/market/market.service.ts
+++ src/market/market.service.ts
@@ -1,20 +1,26 @@
 import { Injectable } from "@nestjs/common";
 import { UserService } from "../user/user.service";
 import { markets } from "./data/market";
+import { cloneDeep } from "lodash";
 
 @Injectable()
 export class MarketService {
-  constructor(
-    private userService: UserService
-  ) {}
+  constructor(private userService: UserService) {}
 
   getMarket(id: string) {
     return markets.find((market) => market.id === id);
   }
 
   async getMarketWithReputation(marketId: string, userId: number) {
-    const market = await this.getMarket(marketId);
+    const originalMarket = await this.getMarket(marketId);
+    if (!originalMarket) {
+      throw new Error(`Market with id ${marketId} not found`);
+    }
+
+    // Create a deep copy of the market to avoid modifying the original data
+    const market = cloneDeep(originalMarket);
     const user = await this.userService.findOne(userId);
+
     market.products.forEach((product) => {
       // @note remove reputation for now
       // product.price = product.price * (1 - user.reputation / 100);
@@ -27,7 +33,6 @@ export class MarketService {
         product.price = product.price * discount;
       }
     });
-
     return market;
   }
 }

feb42ea12aa29b6d7a63826dfd2f621d63033bff
XORs33r <bugislife@proton.me>
2024-07-13 18:05:14 +0800

    fix: add user module to auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,7 +5,7 @@ import { AuthController } from "./auth.controller";
 import { JwtModule } from "@nestjs/jwt";
 import { jwtSecret } from "../config/env";
 import { JwtStrategy } from "./jwt.strategy";
-import { UserService } from "src/user/user.service";
+import { UserModule } from "../user/user.module";
 
 @Module({
   imports: [
@@ -14,6 +14,7 @@ import { UserService } from "src/user/user.service";
       secret: jwtSecret,
       signOptions: { expiresIn: "1d" },
     }),
+    UserModule,
   ],
   providers: [AuthService, JwtStrategy],
   controllers: [AuthController],

c710914315a57de1860135af59a900a501f0672f
Alko89 <alefer89@gmail.com>
2024-08-20 18:41:04 +0200

    fix: base customer limit 500

--- src/user/user.const.ts
+++ src/user/user.const.ts
@@ -2,7 +2,7 @@ export const STARTING_CASH = 1000;
 export const REFERRAL_CASH = 100;
 
 export const BASE_CUSTOMER_NEEDS = 1;
-export const BASE_CUSTOMER_LIMIT = 1000;
+export const BASE_CUSTOMER_LIMIT = 500;
 
 export const BASE_LAB_PLOT_PRICE = 989;
 export const LAB_PLOT_PRICE_MULTIPLIER = 4;

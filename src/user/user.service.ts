5101f6a6dd46d96a30d9a2d2a2fabe0703386f7b
Alko89 <alefer89@gmail.com>
2024-09-23 18:26:01 +0200

    fix: bot id

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -286,7 +286,7 @@ export class UserService {
       }
 
       bots.push({
-        id: faker.number.bigInt(),
+        id: faker.number.int(),
         username: faker.internet.userName({
           firstName: faker.person.firstName(),
         }),
